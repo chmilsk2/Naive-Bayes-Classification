691a778423e5ff8d7691bee948233e43cda2d932
Troy Chmieleski <chmilsk2@illinois.edu>
2013-11-09 16:39:03 -0600

    created Xcode Project

---
+++ Naive Bayes Classification/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Naive Bayes Classification
+//
+//  Created by Troy Chmieleski on 11/9/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ Naive Bayes ClassificationTests/Naive_Bayes_ClassificationTests.m
@@ -0,0 +1,34 @@
+//
+//  Naive_Bayes_ClassificationTests.m
+//  Naive Bayes ClassificationTests
+//
+//  Created by Troy Chmieleski on 11/9/13.
+//  Copyright (c) 2013 Troy Chmieleski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Naive_Bayes_ClassificationTests : XCTestCase
+
+@end
+
+@implementation Naive_Bayes_ClassificationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
